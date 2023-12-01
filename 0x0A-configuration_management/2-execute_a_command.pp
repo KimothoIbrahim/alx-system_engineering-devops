dece083e08b32156da6b200272c5cfed334e9300
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-01 13:45:35 +0300

    Pushed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
-# Create a manifest file
+# Create a manifest file killing the process killmenow
 
-exec { 'killmenow':
-  command     => 'pkill -f killmenowe', # Replace 'my_process_name' with the actual process name or pattern
-  path        => ['/bin', '/usr/bin'],        # Adjust the path as needed
-  onlyif      => 'pgrep -f killmenow',  # Check if the process is running before attempting to kill it
-  refreshonly => true,
+exec { 'name':
+  command => '/usr/bin/pkill killmenow'
 }

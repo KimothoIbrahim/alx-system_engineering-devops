08d3171c3832e7dd565ced9bee9f0aea540043cd
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-01 11:28:51 +0300

    Work on working flask installer

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,11 @@ package{ 'python3':
   ensure => 'installed'
 }
 
+package{ 'python3-pip':
+  ensure  => 'installed',
+  require => Package['python3']
+}
+
 package{ 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'

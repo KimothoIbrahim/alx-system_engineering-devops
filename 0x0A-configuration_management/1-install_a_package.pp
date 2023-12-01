c1226929ab95a69c46ae640a43b2f61a729d489a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-01 10:52:41 +0300

    Work on working flask installer

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,11 +4,13 @@ exec {'apt-get update':
   command => '/usr/bin/apt-get update'
 }
 
+#install pip3
 package{ 'python3-pip':
   ensure  => 'installed',
   require => Exec['apt-get update']
 }
 
+#using pip3 install flask
 package{ 'Flask':
   ensure  => '2.1.0',
   provider => 'pip3'

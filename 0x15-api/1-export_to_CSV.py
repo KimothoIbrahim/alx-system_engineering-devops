38798be6437e67cbbeb893ddc215e01ae9a7128f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-05 21:13:41 +0300

    Edit style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,17 +8,21 @@ if __name__ == "__main__":
 
     userid = sys.argv[1]
 
-    res = requests.get(f"https://jsonplaceholder.typicode.com/users/{userid}")
-    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')
+    res = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{userid}")
+    r = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{userid}/todos')
 
     userDetails = res.json()
     fullList = r.json()
     csv_file = f'{userid}.csv'
 
     print(fullList[0])
-    
+
     with open(csv_file, 'w', newline='') as csvW:
         csv_writer = csv.writer(csvW, quoting=csv.QUOTE_ALL)
 
         for task in fullList:
-            csv_writer.writerow([f"{userDetails.get('id')}", userDetails.get('username'), task.get('completed'), task.get('title')])
+            csv_writer.writerow(
+                [f"{userDetails.get('id')}", userDetails.get('username'),
+                    task.get('completed'), task.get('title')])

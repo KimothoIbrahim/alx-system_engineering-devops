11036275ba9c60507b4f95120d4a032c7e1fa4ed
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-01-05 21:22:39 +0300

    edited

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,14 +9,19 @@ if __name__ == "__main__":
 
     userid = sys.argv[1]
 
-    res = requests.get(f"https://jsonplaceholder.typicode.com/users/{userid}")
-    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')
+    res = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{userid}")
+    r = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{userid}/todos')
 
     userDetails = res.json()
     fullList = r.json()
     json_file = f'{userid}.json'
 
-    newList = [{'task': task.get('title'), 'completed': task.get('completed'), 'username': userDetails.get('username')} for task in fullList]
+    newList = [
+            {'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': userDetails.get('username')} for task in fullList]
 
     with open(json_file, 'w', encoding='utf-8') as jsonfile:
         json.dump({userDetails.get('id'): newList}, jsonfile)

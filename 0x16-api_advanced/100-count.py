19ca3f6c8cea88ca0209573a8f644f0fdbdac651
KimothoIbrahim <kimothoibrahim@yahoo.com>
2025-02-22 20:16:03 +0300

    searching terms

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Advanced API calling
+3. Count it!
+"""
+
+
+def count_words(subreddit, word_list, params='', all_words=[]):
+    """match given word phrases present in the api"""
+    import json
+    import requests
+    import sys
+    from collections import Counter
+
+    try:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+        if response.status_code == 200:
+            for post in response.json()['data']['children']:
+                count = 0
+                all_words.extend([word for word in post['data']['title'].split(" ") if word in word_list ])
+
+
+            params = {'after': response.json()['data']['after']}
+
+            if response.json()['data']['after']:
+                return count_words(subreddit, word_list, params, all_words)
+
+            counter = Counter(all_words)
+            for word in counter:
+                sys.stdout.write(f"{word}: {counter[word]}\n")
+        else:
+            return None
+
+    except Exception as e:
+        print(e)
+
